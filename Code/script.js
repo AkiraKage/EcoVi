55d2f18525edac37db6811d328034ad229da23a1
musta571 <giovannigiomontagna@gmail.com>
2024-02-20 20:00:50 +0100

    aa

--- Code/script.js
+++ Code/script.js
@@ -1,5 +1,6 @@
 const images = ['./Img/bottle.png','./Img/box.png','./Img/plastic bottle.png','./Img/garbage_black.png']
 
+
 const matrix = [];
 for (let i = 0; i < 5; i++) {
     matrix.push([]);
@@ -14,7 +15,7 @@ for (let i = 0; i < 5; i++) {
         matrix[i].push(randomIndex);
     }
 }
-
+/*
 const table = document.querySelector('table');
 for (let i = 0; i < 5; i++) {
     const row = document.createElement('tr');
@@ -27,3 +28,64 @@ for (let i = 0; i < 5; i++) {
     }
     table.appendChild(row);
 }
+*/
+
+let row=5
+let col=5
+document.getElementById("contenitore").style.gridTemplateColumns = "repeat(" + col + ", 1fr)"
+
+let array = []
+for (let i = 0; i < row * col ; i++) {
+  array.push(i)
+
+}
+
+
+//disegna le card
+console.log(array)
+for (let i = 0; i < array.length; i++) {
+    let randomIndex2 = Math.floor(Math.random() * 4);
+    let s = "<div class='card'><img src='./Img/bottle.png' ></div>" 
+    let a = "<div class='card'><img src='./Img/box.png' alt='Immagine'></div>"
+    let b = "<div class='card'><img src='./Img/plastic bottle.png' alt='Immagine'></div>"
+    let c = "<div class='card'><img src='./Img/garbage_black.png' alt='Immagine'></div>"
+    if(randomIndex2 == 0) {
+        document.getElementById("contenitore").innerHTML += s; 
+        }
+        else if(randomIndex2 == 1) {
+            document.getElementById("contenitore").innerHTML += a; 
+            }
+        else    if(randomIndex2 == 2) {
+                document.getElementById("contenitore").innerHTML += b; 
+                }
+              else  if(randomIndex2 == 3) {
+                    document.getElementById("contenitore").innerHTML += c; 
+                    }
+
+    
+
+}
+
+/*
+for (let i = 0; i < array.length; i++) {
+    let randomIndex2 = Math.floor(Math.random() * images.length);
+    if(randomIndex2 == 0) {
+    let s = "<div class='card'><img src='./Img/bottle.png' alt='Immagine'></div>"
+    document.getElementById("contenitore").innerHTML += s; 
+    }
+    else if(randomIndex2 == 1) {
+        let s = "<div class='card'><img src='./Img/box.png' alt='Immagine'></div>"
+        document.getElementById("contenitore").innerHTML += s; 
+        }
+    else    if(randomIndex2 == 2) {
+            let s = "<div class='card'><img src='./Img/plastic bottle.png' alt='Immagine'></div>"
+            document.getElementById("contenitore").innerHTML += s; 
+            }
+          else  if(randomIndex2 == 3) {
+                let s = "<div class='card'><img src='./Img/garbage_black.png' alt='Immagine'></div>"
+                document.getElementById("contenitore").innerHTML += s; 
+                }
+    document.getElementById("contenitore").innerHTML += s;
+
+}
+*/
