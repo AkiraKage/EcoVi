2ba29dc38aa35083805908e219f61630e627595d
akira_kage <alok95ita@gmail.com>
2024-02-28 18:55:24 +0100

    slight modifies on non adiacent icon click effect

--- Code/script.js
+++ Code/script.js
@@ -9,12 +9,12 @@ for (let i = 0; i < col; i++) {
     matrix.push([]);
     for (let j = 0; j < col; j++) {
         let randomIndex;
-        do {
-            randomIndex = Math.floor(Math.random() * 4);
-        } while (
+        while (
             (i >= 2 && matrix[i - 1][j] == randomIndex && matrix[i - 2][j] == randomIndex) ||
             (j >= 2 && matrix[i][j - 1] == randomIndex && matrix[i][j - 2] == randomIndex)
-        )
+        ){
+            randomIndex = Math.floor(Math.random() * 4);
+        }
         matrix[i].push(randomIndex);
     }
 }
@@ -28,7 +28,7 @@ for (let i = 0; i < col; i++) {
 }
 
 let icons = document.getElementsByClassName('cell');
-let i1, j1, pos1, i2, j2, pos2
+let i1, j1, pos1, i2, j2, pos2, click1, click2
 for(let y = 0; y < icons.length; y++){
         icons[y].addEventListener("click", () => {
             const clickedicon = icons[y];
@@ -40,21 +40,24 @@ for(let y = 0; y < icons.length; y++){
                 i1 = i;
                 j1 = j;
                 pos1 = `${i1} ${j1}`
+                click1 = y;
                 console.log(pos1)
                 clickedicon.style.backgroundColor = 'lightblue';
 
             } else if(i1 != undefined && i2 == undefined) {
                 pos2 = `${i} ${j}`
+                click2 = y;
                 if(pos1 != pos2){
                     i2 = i;
                     j2 = j;
-                    clickedicon.style.backgroundColor = 'lightblue';
-
                     if((i1 == i2 && Math.abs(j1 - j2) == 1) || (j1 == j2 && Math.abs(i1 - i2) == 1)) {
+                        clickedicon.style.backgroundColor = 'lightblue';
                         swap(i1,i2,j1,j2);
                         updateGrid();
                     } else {
                         alert("no");
+                        i2 = undefined;
+                        j2 = undefined;
                     }
                 }
             }
