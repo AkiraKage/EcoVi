bc3732c0d3ab29b6a83f5bb73e929431e6da307c
AkiraKage <alok95ita@gmail.com>
2024-03-07 12:35:46 +0100

    ranking fixed

--- Code/rank.js
+++ Code/rank.js
@@ -1,23 +1,34 @@
+let saved = localStorage.getItem('savedplayers');
+let savedPlayers;
+if (saved) {
+    savedPlayers = JSON.parse(saved);
+    console.log(savedPlayers);
+
+    for (let i = 0; i < savedPlayers.length; i++) {
+        let min = i;
+        for (let j = i + 1; j < savedPlayers.length; j++) {
+            if (savedPlayers[j].points < savedPlayers[min].points) {
+                min = j;
+            }
+        }
+        if (min !== i) {
+            swap(min, i);
+        }
+    }
+
+    for (let i = 0; i < 5; i++) {
+        let player = savedPlayers[i];
+        if(player != undefined){
+            document.getElementById(`p${i + 1}`).innerHTML = `${player.name}`;
+            document.getElementById(`pp${i + 1}`).innerHTML = `Punti: ${player.points}`;
+        } else {
+            document.getElementById(`p${i + 1}`).innerHTML = "Non registrato";
+        }
+    }
+}
+
 function swap(a, b) {
     let temp = savedPlayers[a];
     savedPlayers[a] = savedPlayers[b];
     savedPlayers[b] = temp;
-}
-
-let savedPlayers = JSON.parse(localStorage.getItem('savedplayers'));
-for (let i = 0; i < savedPlayers.length; i++) {
-    let min = i;
-    for (let j = i + 1; j < savedPlayers.length; j++) {
-        if (savedPlayers[j].points < savedPlayers[min].points) {
-            min = j;
-        }
-    }
-    if (min !== i) {
-        swap(min, i);
-    }
-}
-
-for (let i = 0; i < 5; i++) {
-    let player = savedPlayers[i];
-    document.getElementById(`p${i + 1}`).value = `${player.name} - Punti: ${player.points}`;
 }
